40d813e5025ae6d1810dfe694b0517e9426b7baf
mariapopa19 <maria_olimp@yahoo.com>
2025-04-11 00:28:39 +0300

    added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './assets/styles/App.css';
 import SignIn from './pages/Authentication/SignIn';
 import { Route, Routes } from 'react-router-dom';
+import SignUp from './pages/Authentication/SignUp';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
